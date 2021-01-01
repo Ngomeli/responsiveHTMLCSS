7850ce2301b72b4970689c5d82f138434b69e94b
Ngomeli <joshuangomeli@gmail.com>
2021-01-01 17:24:06 +0300

    added the close icon on the side nav

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 var menuBtn = document.getElementById("menuBtn");
 var sideNav = document.getElementById("sideNav");
+var menu = document.getElementById("menu");
 
 sideNav.style.right = "-250px"; /*the nav is hidden in the webpage*/
 menuBtn.onclick = function() {
     if (sideNav.style.right == "-250px") {
         sideNav.style.right = "0"; /*the navbar appears on the web page*/
+        menu.src = "images/close.png";
+
     } else {
         sideNav.style.right = "-250px";
+        menu.src = "images/menu.png";
     }
 }
